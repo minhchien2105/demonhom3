f45f44a36875577d846bf8ced165e4c17024bd2b
nguyen chien <minhchien21051997@gmail.com>
2018-10-15 14:35:58 +0700

    Lan 1

---
+++ WebApplication1/WebApplication1/Models/HocSinh.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WebApplication1.Models
+{
+    public class HocSinh
+    {
+        [Key]
+        public string MaHS { get; set; }
+        public string HoTen { get; set; }
+        public bool GioiTinh { get; set; }
+        public virtual Lop Lop { get; set; }
+    }
+}
